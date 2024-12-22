91b57e677e9b72ddf913d30ffaef8293bcf8a9c5
Gowtham <your.email@example.com>
2024-12-22 04:18:15 -0700

    Feat:App routes and Layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { Button } from "./components/ui/button"
-import { Card } from "./components/ui/card"
+import Layout from "./components/Layout"
+import { BrowserRouter,Routes,Route} from "react-router-dom"
+import { ThemeProvider } from "./context/theme-provider"
+import WeatherDashboard from "./pages/weather-dashboard"
+import City from "./pages/city"
+
 
 
 
 function App() {
  
   return (
-    <>
-      <Button>send</Button>
-      
-    </>
+    <BrowserRouter>
+    <ThemeProvider defaultTheme="dark">
+    <Layout>
+      <Routes>
+        <Route path="/"element={<WeatherDashboard/>}/>
+        <Route path="/city/:cityName"element={<City/>}/>
+      </Routes>
+    </Layout>
+    </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
